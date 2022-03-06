5f1cd1b91e840de00d5a4067368f78edd8b11357
marina <mzubtsova@mail.ru>
2022-03-06 00:45:54 -0500

    updated form and hover affect for projects

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ items.forEach(item => {
         item.classList.remove('flip');
     })
 });
+// work on form element
+const formElement = document.querySelector('form');
+console.log(formElement);
+formElement.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // const nameInput = document.getElementById('userName');
+    const emailInput = document.getElementById('userEmail');
+    // const userFeedback = document.getElementById('feedback');
+
+    // const userName = nameInput.value;
+    const userEmail = emailInput.value;
+    // const userMessage = userFeedback.value;
+
+    if (userEmail === "") {
+        alert("Please include your email!");
+    // } else if (userEmail === "") {
+    //     alert('Please include your email!');
+    // } else if (userMessage === "") {
+    //     alert('Please leave us a message. We appreciate your opinion!');
+    } else {
+        alert('Thank you for your email! I will connect you shortly!');
+        nameInput.value = '';
+        emailInput.value = '';
+        userFeedback.value = '';
+    }
+})
